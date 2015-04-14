a313abc617cde367797688ec800bc58cb0a48f88
Lucas Zawacki <lfzawacki@gmail.com>
2015-04-14 15:54:22 -0300

    Mobile client detection and redirection

--- settings.php
+++ settings.php
@@ -13,6 +13,8 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     $settings->add( new admin_setting_configtext( 'BigBlueButtonBNServerURL', get_string( 'bigbluebuttonbnUrl', 'bigbluebuttonbn' ), get_string( 'bbburl', 'bigbluebuttonbn' ), 'http://test-install.blindsidenetworks.com/bigbluebutton/' ) );
     $settings->add( new admin_setting_configtext( 'BigBlueButtonBNSecuritySalt', get_string( 'bigbluebuttonbnSalt', 'bigbluebuttonbn' ), get_string( 'configsecuritysalt', 'bigbluebuttonbn' ), '8cd8ef52e8e101574e400365b55e11a6' ) );
+    $settings->add( new admin_setting_configcheckbox( 'BigBlueButtonBNDetectMobile', get_string( 'bigbluebuttonbnDetectMobile', 'bigbluebuttonbn' ), get_string( 'detectmobile', 'bigbluebuttonbn' ), 1) );
+    $settings->add( new admin_setting_configcheckbox( 'BigBlueButtonBNForceMobile', get_string( 'bigbluebuttonbnForceMobile', 'bigbluebuttonbn' ), get_string( 'forcemobile', 'bigbluebuttonbn' ), 0) );
 }
 
 ?>
